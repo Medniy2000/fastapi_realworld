9c27d60b245da4245920b16ebbdd160a85434e02
Medniy2000 <vvnikitiuk@gmail.com>
2022-09-16 17:41:39 +0300

    update gino settings

--- src/app/extensions/db.py
+++ src/app/extensions/db.py
@@ -1,17 +1,16 @@
 import sqlalchemy
 
-
 from src.app.config.settings import settings
-from gino.ext.starlette import Gino  # noqa
+from gino.ext.starlette import Gino # noqa
 
 metadata = sqlalchemy.MetaData()
 
 db = Gino(
     ssl=None,
+    echo=settings.DEBUG,
+    dsn=settings.POSTGRES_DB_URL,  # URL for SqlAlchemy
     pool_min_size=settings.POSTGRES_POOL_MIN_SIZE,
     pool_max_size=settings.POSTGRES_POOL_MAX_SIZE,
-    dsn=settings.POSTGRES_DB_URL,  # URL for SqlAlchemy
-    echo=settings.DEBUG,
     retry_limit=5,
     retry_interval=3,
 )
